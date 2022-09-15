45befc9c6ca7001b2bfb3c346a0c9113d5010c84
Jose Victor Persset <jpersset@gmail.com>
2022-09-15 17:47:38 -0300

    Add: Create Mock Get Route to test API - Create Controller SuperHero

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -5,5 +5,17 @@ namespace SuperHeroAPI.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     public class SuperHeroController : ControllerBase {
+        [HttpGet]
+
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes() {
+            return new List<SuperHero> {
+                new SuperHero {
+                    Name = "Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
+        }
     }
 }

835aa03f53d806a578835898143fa60edac06a3c
Jose Victor Persset <jpersset@gmail.com>
2022-10-16 10:33:51 -0300

    Add: Enable Cors policy

--- SuperHeroAPI/Program.cs
+++ SuperHeroAPI/Program.cs
@@ -6,6 +6,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => options.AddPolicy(name: "SuperHeroOrigins",
+    policy => {
+        policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+    }));
 
 var app = builder.Build();
 
@@ -14,6 +18,8 @@ if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("SuperHeroOrigins");
 
 app.UseHttpsRedirection();
 

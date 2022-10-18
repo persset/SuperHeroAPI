0cb41b428eff35ba7b6b5ffad6cd7adccd52f1b9
Jose Victor Persset <jpersset@gmail.com>
2022-10-18 17:00:46 -0300

    Add: Add Entity Framework to project and setup connection string

---
+++ SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Data {
+    public class DataContext : DbContext {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        //Needed for the dataset to appear as a table on SQL Management Studio or Whatever u use.
+        public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
+
+    }
+}
